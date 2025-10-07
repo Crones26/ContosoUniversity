841b19dd2788fbb548cb9d2ef28077d41030882d
Cympak <Cympak@DESKTOP-J0NFRBK>
2025-10-07 12:29:02 +0500

    ClassWork DONE

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,4 +1,15 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder
+	.Services
+	.AddDbContext<UniversityContext>
+	(
+		opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"))
+	);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,11 +19,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
+
+IServiceScope scope = app.Services.CreateScope();
+IServiceProvider services = scope.ServiceProvider;
+
+UniversityContext context = services.GetRequiredService<UniversityContext>();
+DbInitializer.Initialize(context);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -21,7 +38,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
